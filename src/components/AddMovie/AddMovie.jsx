3a6b173c18aa235645b06bc806f70917f4ab3d33
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-06 17:03:26 -0500

    refactored form to use single state variable

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -16,22 +16,16 @@ function AddMovie() {
         dispatch({type: 'FETCH_GENRES'})
     }, [])
 
-    // state variables to hold the values entered until submit
-    const [title, setTitle] = useState('');
-    const [genre, setGenre] = useState('');
-    const [poster, setPoster] = useState('');
-    const [description, setDescription] = useState('');
+    const [movieData, setMovieData] = useState({
+        title: '',
+        poster: '',
+        description: '',
+        genre_id: 0
+    })
 
 
     const genres = useSelector(store=>store.genres)
     console.log('genres in add: ', genres);
-
-    const movieData = {
-        title: title,
-        poster: poster,
-        description: description,
-        genre_id: genre
-    }
 
     console.log('movieData: ', movieData);
 
@@ -52,26 +46,23 @@ function AddMovie() {
         <h2>Add a New Movie</h2>
         <form onSubmit={handleSubmit}>
             <label htmlFor='titleInput'>Movie Title</label>
-            <input onChange={(e)=>{setTitle(e.target.value)}}id='titleInput'/>
+            <input onChange={(e)=>{setMovieData({...movieData, title: e.target.value})}}id='titleInput'/>
             <label htmlFor='posterInput'>Poster URL</label>
-            <input onChange={(e)=> {setPoster(e.target.value)}}id='posterInput' />
+            <input onChange={(e)=> {setMovieData({...movieData, poster: e.target.value})}}id='posterInput' />
             <label htmlFor='descriptionInput'>Description/Synopsis</label>
-            <textarea cols={40} rows={4} onChange={(e)=> {setDescription(e.target.value)}}id='descriptionInput'></textarea>
+            <textarea cols={40} rows={4} onChange={(e)=>{setMovieData({...movieData, description: e.target.value})}} id='descriptionInput'></textarea>
             {/* dropdown menu for genres */}
-            <select onChange={(e) => {setGenre(e.target.value)}} name='Genre' id='genre'>
+            <select onChange={(e) => {setMovieData({...movieData, genre_id: e.target.value})}} name='Genre' id='genre'>
                 <option value=''>Please select a Genre</option>
                 {genres.map((genre, i) => (
                     <option  key={i} value={`${genre.id}`}>{genre.name}</option>
                 ))}
             </select>
             <button type="submit">Add Movie</button>
-
+            <button onClick={()=> {history.push('/')}}>Cancel</button>
         </form>
-        
-     
         </>
     )
-
 }
 
 export default AddMovie;
